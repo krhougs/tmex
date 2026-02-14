8e46ffda36dda2d3a25c097276561498341191ab
Debian <gh@kt.je>
2026-02-14 16:10:36 +0000

    chore: colors

--- apps/fe/src/components/terminal/theme.ts
+++ apps/fe/src/components/terminal/theme.ts
@@ -1,66 +1,68 @@
 /**
  * seoul256.vim color scheme
  * https://github.com/junegunn/seoul256.vim
+ * https://github.com/mikker/seoul256-iTerm
  *
- * rgb_map from seoul256.vim (non-sRGB version for macOS/iTerm2)
+ * Dark theme uses seoul256-iTerm colors
+ * Light theme uses seoul256.vim colors
  */
 
 /**
  * seoul256-light - Seoul Colors Light
- * Background: 253 (#E1E1E1), Foreground: 239 (#616161)
+ * From seoul256.vim: Background 253 (#E1E1E1), Foreground 239 (#616161)
  */
 export const XTERM_THEME_LIGHT = {
   background: '#e1e1e1',
   foreground: '#616161',
   cursor: '#616161',
   selectionBackground: 'rgba(97, 97, 97, 0.25)',
-  // ANSI colors for light theme
-  black: '#171717',      // 233
-  red: '#bf2172',        // 125 (magenta-red)
-  green: '#009799',      // 30
-  yellow: '#9a7200',     // 94
-  blue: '#007299',       // 24
-  magenta: '#9b1d72',    // 89
-  cyan: '#007173',       // 23
-  white: '#d9d9d9',      // 252
+  // ANSI colors
+  black: '#171717',
+  red: '#bf2172',
+  green: '#009799',
+  yellow: '#9a7200',
+  blue: '#007299',
+  magenta: '#9b1d72',
+  cyan: '#007173',
+  white: '#d9d9d9',
   // Bright colors
-  brightBlack: '#4e4e4e',     // 239
-  brightRed: '#e12672',       // 161
-  brightGreen: '#00bddf',     // 38
-  brightYellow: '#ffdd00',    // 220
-  brightBlue: '#7299bc',      // 67
-  brightMagenta: '#e17899',   // 168
-  brightCyan: '#6fbcbd',      // 73
-  brightWhite: '#f1f1f1',     // 255
+  brightBlack: '#4e4e4e',
+  brightRed: '#e12672',
+  brightGreen: '#00bddf',
+  brightYellow: '#ffdd00',
+  brightBlue: '#7299bc',
+  brightMagenta: '#e17899',
+  brightCyan: '#6fbcbd',
+  brightWhite: '#f1f1f1',
 };
 
 /**
- * seoul256 - Seoul Colors Dark
- * Background: 237 (#4B4B4B), Foreground: 252 (#D9D9D9)
+ * seoul256-dark - Seoul Colors Dark
+ * From seoul256-iTerm: https://github.com/mikker/seoul256-iTerm
  */
 export const XTERM_THEME_DARK = {
-  background: '#4b4b4b',
-  foreground: '#d9d9d9',
-  cursor: '#d9d9d9',
-  selectionBackground: 'rgba(217, 217, 217, 0.25)',
-  // ANSI colors for dark theme
-  black: '#171717',      // 233
-  red: '#9b1300',        // 88
-  green: '#006f00',      // 22
-  yellow: '#9a7200',     // 94
-  blue: '#007299',       // 24
-  magenta: '#9b1d72',    // 89
-  cyan: '#007173',       // 23
-  white: '#d9d9d9',      // 252
-  // Bright colors
-  brightBlack: '#565656',     // 238
-  brightRed: '#e12672',       // 161
-  brightGreen: '#719872',     // 65
-  brightYellow: '#ffdd00',    // 220
-  brightBlue: '#70bddf',      // 74
-  brightMagenta: '#e17899',   // 168
-  brightCyan: '#97dddf',      // 116
-  brightWhite: '#f1f1f1',     // 255
+  background: '#262626',
+  foreground: '#d0d0d0',
+  cursor: '#c5c5c5',
+  selectionBackground: 'rgba(197, 197, 197, 0.25)',
+  // ANSI colors (from iTerm colors)
+  black: '#000000',
+  red: '#ba3c3c',
+  green: '#5d876d',
+  yellow: '#d5a54e',
+  blue: '#887c8d',
+  magenta: '#cd6d6d',
+  cyan: '#618484',
+  white: '#cfcdc3',
+  // Bright colors (from iTerm colors)
+  brightBlack: '#000000',
+  brightRed: '#ea7171',
+  brightGreen: '#7aab7a',
+  brightYellow: '#d1d194',
+  brightBlue: '#afa3b5',
+  brightMagenta: '#e29f9f',
+  brightCyan: '#a0aea3',
+  brightWhite: '#d0d0d0',
 };
 
 export const XTERM_FONT_FAMILY =
